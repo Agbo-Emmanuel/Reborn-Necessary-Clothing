b58e1b8923c71e651ab76593a3c529413bcc7719
Agbo-Emmanuel <agboe4102@gmail.com>
2025-06-25 17:28:40 +0100

    ll

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -22,80 +22,93 @@ export const ProductCard = ({limit, showLastFour, width}) => {
         {
             id: 1,
             image: product_image1,
-            name: "21WN reversible angora cardigan",
-            price: "$120",
+            name: "Tribe & True",
+            desc: "Rooted in heritage, styled with edge,Tribe & True is an Afro-inspired kaftan top that celebrates the unique, expresses confidence, and radiates pure happy energy. Bold prints, bold spirit.",
+            tag: "Culture in color. Confidence in every thread.",
         },
         {
             id: 2,
             image: product_image2,
             name: "21WN reversible angora cardigan",
-            price: "$120",
+            desc: "",
+            tag: "$120",
         },
         {
             id: 3,
             image: product_image3,
-            name: "21WN reversible angora cardigan",
-            price: "$120",
+            name: "Cocktail Bloom",
+            desc: "A burst of color and confidence, Cocktail Bloom is the ultimate cocktail dress for the unique, the happy, and the confident. With playful drapes and a sleek silhouette, it’s your go-to for stylish nights and unforgettable entrances.",
+            tag: "Sip joy. Wear confidence. Be unforgettable.",
         },
         {
             id: 4,
             image: product_image4,
             name: "21WN reversible angora cardigan",
-            price: "$120",
+            desc: "",
+            tag: "$120",
         },
         {
             id: 5,
             image: product_image5,
-            name: "21WN reversible angora cardigan",
-            price: "$120",
+            name: "Sunlit Bloom",
+            desc: "Crafted for the unique, made to inspire confidence, and radiating happy vibes. Sunlit Bloom is a burst of sunshine in dress form. With vibrant floral appliqué on bold yellow and a striking magenta twist, it’s your daytime statement of joy.",
+            tag: "Be the sunshine. Bloom with confidence.",
         },
         {
             id: 6,
             image: product_image6,
-            name: "21WN reversible angora cardigan",
-            price: "$120",
+            name: "Berry Royale",
+            desc: "Rich, refined, and unforgettable — Berry Royale is a majestic gown for the unique woman who radiates confidence and walks in her happy power. Its deep berry hue and elegant structure make it a timeless showstopper.",
+            tag: "Be royal. Be radiant. Be you.",
         },
         {
             id: 7,
             image: product_image7,
-            name: "21WN reversible angora cardigan",
-            price: "$120",
+            name: "Fierce Petal",
+            desc: "Make a statement in Fierce Petal, a floral-textured jumpsuit in hot pink, softened with lace, and tailored for the modern woman who blooms with confidence, shines uniquely, and walks in happy power.",
+            tag: "Bloom bold. Live fierce.",
         },
         {
             id: 8,
             image: product_image8,
             name: "21WN reversible angora cardigan",
-            price: "$120",
+            desc: "",
+            tag: "$120",
         },
         {
             id: 9,
             image: product_image9,
             name: "21WN reversible angora cardigan",
-            price: "$120",
+            desc: "",
+            tag: "$120",
         },
         {
             id: 10,
             image: product_image10,
             name: "21WN reversible angora cardigan",
-            price: "$120",
+            desc: "",
+            tag: "$120",
         },
         {
             id: 11,
             image: product_image11,
             name: "21WN reversible angora cardigan",
-            price: "$120",
+            desc: "",
+            tag: "$120",
         },
         {
             id: 12,
             image: product_image12,
             name: "21WN reversible angora cardigan",
-            price: "$120",
+            desc: "",
+            tag: "$120",
         },
         {
             id: 13,
             image: product_image13,
             name: "21WN reversible angora cardigan",
-            price: "$120",
+            desc: "",
+            tag: "$120",
         },
     ]
 
@@ -116,8 +129,9 @@ export const ProductCard = ({limit, showLastFour, width}) => {
                         <img src={e.image} alt=''/>
                     </div>
                     <div className='product_card_text_container'>
-                        <p>{e.name}</p>
-                        {/* <h6>{e.price}</h6> */}
+                        <h3>{e.name}</h3>
+                        <p>{e.desc}</p>
+                        <h6>#{e.tag}</h6>
                     </div>
                 </div>
             ))
